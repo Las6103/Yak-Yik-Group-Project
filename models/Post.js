f6f5be8260488b2cea0f50b3be6b59fe4b733fc3
LeonardoSilva <apx88.0@gmail.com>
2020-06-10 14:59:13 -0400

    updated model

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,20 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
+const Reply = new Schema({
+  reply: {
+    type: String,
+    required: true,
+  },
+});
+
 const Post = new Schema({
   post: {
     type: String,
     required: true,
   },
+
+  reply: [Reply]
 });
 
 module.exports = mongoose.model("Post", Post);
