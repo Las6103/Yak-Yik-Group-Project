4cbdaa54cb4667bea35b4bd76ef59e7fb77534cd
Brian Avjean <bavjean@gmail.com>
2020-06-10 14:48:55 -0400

    added create

--- routes/postroutes.js
+++ routes/postroutes.js
@@ -9,4 +9,11 @@ router.get("/", (req, res) => {
   Posts.find({}).then((posts) => res.json(posts));
 });
 
+// Create
+router.post("/", (req, res) => {
+  Posts.create(req.body)
+    .then((posts) => res.json(posts))
+    .catch((err) => console.log("Error :", err));
+});
+
 module.exports = router;
