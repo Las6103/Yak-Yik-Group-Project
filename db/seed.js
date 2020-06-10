a827491b9c71b09f29bab422e9a25de5234b6db4
Brian Avjean <bavjean@gmail.com>
2020-06-10 15:18:11 -0400

    reworked seeding to nest replies

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,8 @@
 const Post = require("../models/Post")
-const Reply = require("../models/Reply")
 const postData = require("./OP.json")
-const replyData = require("./RP.json")
 
 Post.deleteMany({}).then(() => {
     Post.create(postData)
-    .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-})
-
-Reply.deleteMany({}).then(() => {
-    Reply.create(replyData)
     .then((data) => {
         console.log(data);
       })
